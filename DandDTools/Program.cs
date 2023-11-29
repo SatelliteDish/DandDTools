5d32c81dbba559689540a2e8020ec22e75e7f4b6
SatelliteDish <94733164+SatelliteDish@users.noreply.github.com>
2023-11-29 22:35:33 +0000

    initial commit

---
+++ DandDTools/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program {
+
+    static void Main(string[] args) {
+        List<DamageData> damageDatas = new List<DamageData>();
+        Random rng = new Random();
+        damageDatas.Add(new DamageData(1,6,DamageData.DamageType.Piercing, rng));
+        damageDatas.Add(new DamageData(1,8,DamageData.DamageType.Fire, rng));
+        MeleeWeapon weapon = new MeleeWeapon(name: "Shortsword",damages: damageDatas, modifier: 1, WeaponType.Martial);
+        weapon.RollDamage();
+    }
+}
