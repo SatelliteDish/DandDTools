9af9a1f3c08cf1ccb91ed0fe0397bc6eeaa68c16
SatelliteDish <94733164+SatelliteDish@users.noreply.github.com>
2023-12-02 02:55:20 +0000

    fix bug

--- DandDTools/Program.cs
+++ DandDTools/Program.cs
@@ -1,11 +1,11 @@
 ﻿class Program {
 
     static void Main(string[] args) {
-        List<DamageData> damageDatas = new List<DamageData>();
-        Random rng = new Random();
-        damageDatas.Add(new DamageData(1,6,DamageData.DamageType.Piercing, rng));
-        damageDatas.Add(new DamageData(1,8,DamageData.DamageType.Fire, rng));
-        MeleeWeapon weapon = new MeleeWeapon(name: "Shortsword",damages: damageDatas, modifier: 1, WeaponType.Martial);
+        WeaponList weaponList = WeaponList.Instance;
+        MeleeWeapon weapon = weaponList.Shortsword();
+        RangedWeapon rangedWeapon = weaponList.Longbow();
         weapon.RollDamage();
+        rangedWeapon.RollDamage();
+        Console.WriteLine($"name is {rangedWeapon.Name} and range is {rangedWeapon.NormalRange}-{rangedWeapon.MaxRange}");
     }
 }
