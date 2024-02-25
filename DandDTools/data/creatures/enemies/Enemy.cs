aeced25353b9123cdd0b7fc14076b99e7f4103e5
Michael V <michael.a.j.vitale@gmail.com>
2024-02-24 17:40:46 -0800

    overhauled attack system. Removed attack energy and used delegates instead. Marked types as nullable

--- DandDTools/data/creatures/enemies/Enemy.cs
+++ DandDTools/data/creatures/enemies/Enemy.cs
@@ -1,13 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
 public class Enemy: Creature {
     public required float Challenge { get; init; }
-    int maxAttackEnergy = 0;
-
-    public int AttackEnergy { get; set; }
 
     [SetsRequiredMembers]
     public Enemy(
         string name,
+        string species,
         int hp,
         int ac,
         int speed,
@@ -18,14 +16,15 @@ public class Enemy: Creature {
         float challenge,
         Dictionary<SenseType, int> senses,
         List<LanguageType> languages,
-        List<DamageType> resistances = null,
-        List<DamageType> immunites = null,
-        List<Conditions> conditionImmunities = null,
-        List<PassiveAbilityList.PassiveAbility> passiveAbilities = null,
-        Dictionary<Weapon,int> attacks = null,
-        int attackEnergy = 0
+        List<DamageType>? resistances = null,
+        List<DamageType>? immunites = null,
+        List<Conditions>? conditionImmunities = null,
+        List<PassiveAbilityList.PassiveAbility>? passiveAbilities = null,
+        List<Weapon>? attacks = null,
+        ICombatParticipant.Turn? turn = null
     ):base(
         name: name,
+        species: species,
         hp: hp,
         ac:ac,
         speed: speed,
@@ -39,20 +38,14 @@ public class Enemy: Creature {
         immunites: immunites,
         conditionImmunities: conditionImmunities,
         passiveAbilities: passiveAbilities,
-        attacks: attacks
+        attacks: attacks,
+        turn: turn
         ) {
         Challenge = challenge;
-        maxAttackEnergy = attackEnergy;
-        AttackEnergy = attackEnergy;
     }
     public void Attack(int index) {
-        Weapon weapon = weapons[index];
-        if(!Attacks.ContainsKey(weapon)) throw new ArgumentException($"{Name} cannot use weapon {weapon}, {Name} does not have that weapon!");
-        if(AttackEnergy < Attacks[weapon]) {
-            Console.WriteLine("Cannot attack, out of energy!");
-            return;
-        }
-        AttackEnergy -= Attacks[weapon];
+        Weapon weapon = Attacks[index];
+        if(!Attacks.Contains(weapon)) throw new ArgumentException($"{Name} cannot use weapon {weapon}, {Name} does not have that weapon!");
         List<(DamageType type, int count)> damage = weapon.RollDamage();
         foreach((DamageType type, int count) in damage) {
             Console.WriteLine($"Deals {count} {type} damage!");
@@ -60,16 +53,15 @@ public class Enemy: Creature {
     }
 
     public void Attack(string name) {
-        if(weapons.Count < 1) throw new InvalidOperationException($"{Name} cannot attack!");
-        Weapon weapon = null;
-        foreach(Weapon wpn in weapons) {
+        if(Attacks.Count < 1) throw new InvalidOperationException($"{Name} cannot attack!");
+        Weapon? weapon = null;
+        foreach(Weapon wpn in Attacks) {
             if(wpn.Name == name) {
                 weapon = wpn;
                 break;
             }
         }
         if(weapon == null) throw new ArgumentException($"{Name} does not have {name}");
-        AttackEnergy -= Attacks[weapon];
         List<(DamageType type, int count)> damage = weapon.RollDamage();
         foreach((DamageType type, int count) in damage) {
             Console.WriteLine($"Deals {count} {type} damage!");
--- DandDTools/data/creatures/enemies/EnemyList.cs
+++ DandDTools/data/creatures/enemies/EnemyList.cs
@@ -1,10 +1,13 @@
+using System.Runtime.CompilerServices;
+
 public class EnemyList {
     public readonly Random rng = new Random();
     public readonly PassiveAbilityList passiveAbilities = new PassiveAbilityList();
     public readonly WeaponList weapons = new WeaponList();
     public Enemy Goblin(string name) {
-        return new Enemy(
+        Enemy gob =  new Enemy(
             name: name,
+            species: "Goblin",
             hp: rng.Next(2,13),
             ac: 15,
             speed: 30,
@@ -30,8 +33,26 @@ public class EnemyList {
             senses: new Dictionary<SenseType, int> {{SenseType.Darkvision, 60},{SenseType.PassivePerception, 9}},
             languages: new List<LanguageType>{LanguageType.Common,LanguageType.Goblin},
             passiveAbilities: new List<PassiveAbilityList.PassiveAbility>{passiveAbilities.NimbleEscape()},
-            attacks: new Dictionary<Weapon, int> {{weapons.Scimitar(),1},{weapons.Shortbow(),1}},
-            attackEnergy: 1
+            attacks: new List<Weapon> {
+                weapons.Scimitar(),
+                weapons.Shortbow()
+            }
         );
+        gob.TakeTurn = delegate () {
+                Console.WriteLine("Choose your attack:");
+                for(int i = 0; i < gob.Attacks.Count; i++) {
+                    Console.WriteLine($"{i + 1}: {gob.Attacks[i].Name}");
+                }
+                int choice;
+                while(true) {
+                    try {
+                        choice = Convert.ToInt32(Console.ReadLine()) - 1;
+                    } catch { continue; }
+                    if(choice < 0 | choice > gob.Attacks.Count - 1) continue;
+                    break;
+                }
+                gob.Attack(choice);
+        };
+        return gob;
     }
 }
