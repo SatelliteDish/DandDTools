7d99a88c60a6107ef5fa7d3c19099b546331ccd4
Michael V <michael.a.j.vitale@gmail.com>
2024-02-28 18:14:39 -0800

    Added bugbears

--- DandDTools/data/creatures/enemies/EnemyList.cs
+++ DandDTools/data/creatures/enemies/EnemyList.cs
@@ -1,14 +1,13 @@
 using System.Runtime.CompilerServices;
 
 public class EnemyList {
-    public readonly Random rng = new Random();
     public readonly PassiveAbilityList passiveAbilities = new PassiveAbilityList();
     public readonly WeaponList weapons = new WeaponList();
     public Enemy Goblin(string name) {
         Enemy gob =  new Enemy(
             name: name,
             species: "Goblin",
-            maxHP: rng.Next(2,13),
+            maxHP: Dice.Roll(2,Dice.Type.D6),
             ac: 15,
             speed: 30,
             size: CreatureSize.Small,
@@ -71,5 +70,76 @@ public class EnemyList {
                 gob.Attack(attack, gob.Targets[target]);
         };
         return gob;
+    }
+    public Enemy Bugbear(string name) {
+        Enemy bug =  new Enemy(
+            name: name,
+            species: "Bugbear",
+            maxHP: Dice.Roll(5,Dice.Type.D8) + 5,
+            ac: 16,
+            speed: 30,
+            size: CreatureSize.Medium,
+            type: CreatureType.Humanoid,
+            stats: new StatData(
+                strength: 15,
+                dexterity: 14,
+                constitution: 13,
+                intelligence: 8,
+                wisdom: 11,
+                charisma: 9
+            ),
+            savingThrows: new StatData(
+                strength: 0,
+                dexterity: 0,
+                constitution: 0,
+                intelligence: 0,
+                wisdom: 0,
+                charisma: 0
+            ),
+            challenge: 1f,
+            senses: new Dictionary<SenseType, int> {{SenseType.Darkvision, 60},{SenseType.PassivePerception, 10}},
+            proficiency: 2,
+            languages: new List<LanguageType>{LanguageType.Common,LanguageType.Goblin},
+            passiveAbilities: new List<PassiveAbilityList.PassiveAbility>{
+                passiveAbilities.Brute("Bugbear"),
+                passiveAbilities.SupriseAttack("Bugbear"),                
+            },
+            attacks: new List<Weapon> {
+                weapons.Morningstar(),
+                weapons.Javelin()
+            }
+        );
+        bug.TakeTurn = delegate () {
+                if(bug.Targets.Count == 0 | bug.Attacks.Count == 0) {
+                    Console.WriteLine("Cannot Attack!");
+                    return;
+                }
+                Console.WriteLine("Choose your attack:");
+                for(int i = 0; i < bug.Attacks.Count; i++) {
+                    Console.WriteLine($"{i + 1}: {bug.Attacks[i].Name}");
+                }
+                int attack;
+                while(true) {
+                    try {
+                        attack = Convert.ToInt32(Console.ReadLine()) - 1;
+                    } catch { continue; }
+                    if(attack < 0 | attack > bug.Attacks.Count - 1) continue;
+                    break;
+                }
+                Console.WriteLine("Choose your target:");
+                for(int i = 0; i < bug.Targets.Count; i++) {
+                    Console.WriteLine($"{i + 1}: {bug.Targets[i].Name}");
+                }
+                int target;
+                while(true) {
+                    try {
+                        target = Convert.ToInt32(Console.ReadLine()) - 1;
+                    } catch { continue; }
+                    if(target < 0 | target > bug.Targets.Count - 1) continue;
+                    break;
+                }
+                bug.Attack(attack, bug.Targets[target]);
+        };
+        return bug;
     }
 }
