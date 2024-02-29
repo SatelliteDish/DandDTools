4d5c41b246f09c07de03f1701bbbe90c946ede69
Michael V <michael.a.j.vitale@gmail.com>
2024-02-28 18:12:22 -0800

    Allowed multiple dice to be rolled at once.

--- DandDTools/data/common/Dice.cs
+++ DandDTools/data/common/Dice.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Dice {
     public enum Type {
+        D1,
         D4,
         D6,
         D8,
@@ -8,17 +11,30 @@ public class Dice {
         D20,
         D100
     }
+    public static int Roll(int count, Type dice) {
+        int total = 0;
+        for(int i = 0; i < count; i++) {
+            total += Roll(dice);
+        }
+        return total;
+    }
     public static int Roll(Type dice) {
         int result = dice switch {
-            Type.D4 => Dice.RollD4(),
-            Type.D6 => Dice.RollD6(),
-            Type.D8 => Dice.RollD8(),
-            Type.D10 => Dice.RollD10(),
-            Type.D12 => Dice.RollD12(),
-            Type.D20 => Dice.RollD20(),
-            Type.D100 => Dice.RollD100()
+            Type.D1 => 1,
+            Type.D4 => RollD4(),
+            Type.D6 => RollD6(),
+            Type.D8 => RollD8(),
+            Type.D10 => RollD10(),
+            Type.D12 => RollD12(),
+            Type.D20 => RollD20(),
+            Type.D100 => RollD100(),
+            _ => throw new ArgumentException("Error, Please supply a valid dice type!"),
         };
         return result;
+    }
+    public static int Roll(int max, int min = 1) {
+        Random rng = new Random();
+        return rng.Next(minValue: min, maxValue: max + 1);
     }
     public static int RollD4() {
         Random rng = new Random();
